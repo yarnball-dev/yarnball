66b758d06a71b299521c6ff87581353388205968
Joshua Shone <josh.shone@gmail.com>
2016-03-01 14:14:15 +0100

    core/web test now covers queries, link counts

--- core/tests/web-test.js
+++ core/tests/web-test.js
@@ -18,6 +18,23 @@ test('web test', function(t) {
   t.equal(web.getLinks().length, 1, 'Web getLinks().length should be 1 after setLink([link], []).');
   t.ok(web.equal(web), 'Web should still compare equal to itself after adding link.');
   t.ok(web.hasLink(link.from, link.via, link.to), 'Web hasLink(link) should return true after setLink([link], []).');
+  
+  t.ok(Node.equal(web.queryOne(link.from, link.via, null),    link.to),   'Querying for one node by giving the from and via of an added link should return the to node.');
+  t.ok(Node.equal(web.queryOne(null,      link.via, link.to), link.from), 'Querying for one node by giving the via and to nodes of an added link should return the from node.');
+  t.ok(Node.equal(web.queryOne(link.from, null,     link.to), link.via),  'Querying for one node by giving the from and to nodes of an added link should return the via node.');
+  
+  t.equal(web.query(link.from, link.via, null).size(), 1,    'Querying for nodes by giving the from and via of an added link should return a set with a size of 1.');
+  t.ok(   web.query(link.from, link.via, null).has(link.to), 'Querying for nodes by giving the from and via of an added link should return a set that contains the to node.');
+  
+  t.equal(web.query(null, link.via, link.to).size(), 1,      'Querying for nodes by giving the via and to of an added link should return a set with a size of 1.');
+  t.ok(   web.query(null, link.via, link.to).has(link.from), 'Querying for nodes by giving the via and to of an added link should return a set that contains the from node.');
+  
+  t.equal(web.query(link.from, null, link.to).size(), 1,     'Querying for nodes by giving the from and to of an added link should return a set with a size of 1.');
+  t.ok(   web.query(link.from, null, link.to).has(link.via), 'Querying for nodes by giving the from and to of an added link should return a set that contains the via node.');
+  
+  t.equal(web.getLinkCount(), 1, 'Performing queries on a web should not change the value returned by getLinkCount().');
+  t.equal(web.getLinks().length, 1, 'Performing queries on a web should not change size of the array returned by getLinks()');
+  
   t.doesNotThrow(function() { web.setLinks([], [link]); }, 'Web setLinks([], [link]) should not throw.');
   t.equal(web.getLinks().length, 0, 'Web getLinks().length should be 0 after setLink([], [link]).');
   t.notOk(web.hasLink(link.from, link.via, link.to), 'Web hasLink(link) should return false after setLink([], [link]).');
